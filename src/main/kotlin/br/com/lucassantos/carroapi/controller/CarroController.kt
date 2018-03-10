e62faebf34f5d2e939f38cc9d891f7ad859ecce1
lucas dos santos <lucas_rafa_94@hotmail.com>
2018-03-10 12:18:36 -0300

    commit

--- src/main/kotlin/br/com/lucassantos/carroapi/controller/CarroController.kt
+++ src/main/kotlin/br/com/lucassantos/carroapi/controller/CarroController.kt
@@ -3,9 +3,7 @@ package br.com.lucassantos.carroapi.controller
 import br.com.lucassantos.carroapi.model.Carro
 import br.com.lucassantos.carroapi.service.CarroService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/carro")
@@ -18,4 +16,13 @@ class CarroController {
         return carroService.buscarTodosCarros();
     }
 
+    @PostMapping
+    fun salvar(@RequestBody carro: Carro){
+        carroService.salvar(carro)
+    }
+
+    @RequestMapping("busca/placa/{placa}")
+        fun buscarPorPlaca(@PathVariable(value="placa") placa: String) : List<Carro>{
+        return carroService.buscarPorPlaca(placa)
+    }
 }
--- src/main/kotlin/br/com/lucassantos/carroapi/model/Carro.kt
+++ src/main/kotlin/br/com/lucassantos/carroapi/model/Carro.kt
@@ -4,11 +4,11 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
-data class Carro (@Id var id: String,
-                  var marca: String,
-                  var modelo: String,
-                  var ano: Int,
-                  var placa: String){
+data class Carro (@Id var id: String?,
+                  var marca: String?,
+                  var modelo: String?,
+                  var ano: Int?,
+                  var placa: String?){
 
     /*constructor() : this("",
                         "",
